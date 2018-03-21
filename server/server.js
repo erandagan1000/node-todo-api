656f7bbe13a9254f6d780cf52a23c31786911298
Eran <eran.bokobza@intel.com>
2018-03-21 11:32:57 +0200

    add route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //remote libs
 var express= require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //local libs
 var {mongoose} = require('./db/mongoose');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     },(e) => {res.status(400)});
 
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id= req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        
+        res.send({todo, status:res.statusCode});
+    }).catch((e) => res.status(400).send());
+
+});
+
 
 app.listen(3000, () => console.log('listening on port 3000'));
 
