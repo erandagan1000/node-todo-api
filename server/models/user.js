7073dcc16f0ea21601d4fd291e47a69ff15d4db1
Eran <eran.bokobza@intel.com>
2018-03-21 16:56:57 +0200

    add POSt /users and auth token

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,58 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
     email:{
         type: String,
         required: true,
         trim:true,
-        minlength: 3
-    }
+        minlength: 3,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,  
+            message: '{VALUE} is not a valid email' 
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    tokens:[{
+        access:{
+            type:String,
+            required: true
 
+        },
+        token:{
+            type:String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+
+    user.tokens.push({access, token});
+    //user.tokens = user.tokens.concat([{access, token}]);  ///ifpush doesnt work
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+//override method to override the default json that is sen to client
+UserSchema.methods.toJSON = function(){
+    var user =this;
+    var userObject = user.toObject();
+    return _.pick(userObject,['_id','email']);
+};
+
+var User = mongoose.model('User',UserSchema);
 
 
 module.exports = {User};
