53a8e564e44e6924327a4ff8aad3159aae2a339a
Eran <eran.bokobza@intel.com>
2018-03-21 10:16:39 +0200

    add tests

--- server/server.js
+++ server/server.js
@@ -17,10 +17,17 @@ app.post('/todos',(req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-        res.status(200);
-    }, (e) => res.send(e));
+    }, (e) =>res.status(400).send(e));
+
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    },(e) => {res.status(400)});
 
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
 
+module.exports = {app};
